740e0eea04dfea3fd6ea2bcb1dea98305d124c97
JRIngram <jr-ingram@hotmail.co.uk>
2021-05-25 20:08:04 +0100

    Remove unused imports

--- client/src/screens/ScreenContainer/ScreenContainer.test.tsx
+++ client/src/screens/ScreenContainer/ScreenContainer.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitFor, findByText } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import ScreenContainer from './ScreenContainer';
 
