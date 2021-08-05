7c0b16c1f74d532523dc7fde3b552ae7e90f6379
JRIngram <jr-ingram@hotmail.co.uk>
2021-08-05 19:46:27 +0100

    Fix linting errors

--- e2e/quizBoxes.ts
+++ e2e/quizBoxes.ts
@@ -1,38 +1,39 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { Selector } from 'testcafe';
 import { dropAllTestCollections } from '../testUtils/testUtils';
 import url from './constants';
 
 const selectors = {
-    // Set up
-    manageNavButton: Selector('p').withText('Manage'),
-    addCardsButton: Selector('#coloured-button-add-cards'),
-    addCardButton: Selector('#coloured-button-add-card'),
-    promptInput: Selector('#prompt'),
-    answerInput: Selector('#answer'),
-    manageQuizzesButton: Selector('#coloured-button-manage-quizzes'),
-    quizNameInput: Selector('#quizName'),
-    quizDescriptionInput: Selector('#quizDescription'),
-    addQuizButton: Selector('#coloured-button-confirm-add-quiz'),
-    confirmEditQuizButton: Selector('#coloured-button-confirm-edit-quiz'),
-    cardOnePrompt: Selector('li').withText('What is the latin name for the "Eastern Gray Squirrel"?'),
-    cardTwoPrompt: Selector('li').withText('What is the latin name for the "Barn Owl"?'),
-    // Study Specific
-    quizTitle: Selector('h1').withText('Latin animal names'),
-    quizReviewHeader: Selector('h2').withText('Quiz Review'),
-    studyNavButton: Selector('p').withText('Study'),
-    startQuizButton: Selector('#coloured-button-start-quiz'),
-    quizAnswerInput: Selector('#answer-input'),
-    quizPrompt: Selector('#quizPrompt'),
-    submitAnswer: Selector('#coloured-button-submit-answer'),
-    correctAnswerButton: Selector('#coloured-button-correct'),
-    incorrectAnswerButton: Selector('#coloured-button-incorrect'),
-    quizScore: Selector('h3').withText('2 correct out of 3 - 66.67%'),
-    finishQuizButton: Selector('#coloured-button-finish-review'),
-    quizBoxDropdown: Selector('#quizBoxes'),
-    quizBoxDropdownOnePlus: Selector('#quizBoxes-1'),
-    quizBoxDropdownTwoPlus: Selector('#quizBoxes-2'),
-    quizBoxDropdownThree: Selector('#quizBoxes-3'),
-}
+  // Set up
+  manageNavButton: Selector('p').withText('Manage'),
+  addCardsButton: Selector('#coloured-button-add-cards'),
+  addCardButton: Selector('#coloured-button-add-card'),
+  promptInput: Selector('#prompt'),
+  answerInput: Selector('#answer'),
+  manageQuizzesButton: Selector('#coloured-button-manage-quizzes'),
+  quizNameInput: Selector('#quizName'),
+  quizDescriptionInput: Selector('#quizDescription'),
+  addQuizButton: Selector('#coloured-button-confirm-add-quiz'),
+  confirmEditQuizButton: Selector('#coloured-button-confirm-edit-quiz'),
+  cardOnePrompt: Selector('li').withText('What is the latin name for the "Eastern Gray Squirrel"?'),
+  cardTwoPrompt: Selector('li').withText('What is the latin name for the "Barn Owl"?'),
+  // Study Specific
+  quizTitle: Selector('h1').withText('Latin animal names'),
+  quizReviewHeader: Selector('h2').withText('Quiz Review'),
+  studyNavButton: Selector('p').withText('Study'),
+  startQuizButton: Selector('#coloured-button-start-quiz'),
+  quizAnswerInput: Selector('#answer-input'),
+  quizPrompt: Selector('#quizPrompt'),
+  submitAnswer: Selector('#coloured-button-submit-answer'),
+  correctAnswerButton: Selector('#coloured-button-correct'),
+  incorrectAnswerButton: Selector('#coloured-button-incorrect'),
+  quizScore: Selector('h3').withText('2 correct out of 3 - 66.67%'),
+  finishQuizButton: Selector('#coloured-button-finish-review'),
+  quizBoxDropdown: Selector('#quizBoxes'),
+  quizBoxDropdownOnePlus: Selector('#quizBoxes-1'),
+  quizBoxDropdownTwoPlus: Selector('#quizBoxes-2'),
+  quizBoxDropdownThree: Selector('#quizBoxes-3'),
+};
 
 // @ts-ignore
 const addQuiz = async (t) => {
@@ -74,13 +75,13 @@ test.before(async (t) => {
   await addQuiz(t);
 })('when studying a quiz, the cards move boxes when marked as correct or incorrect', async (t) => {
   const {
-    studyNavButton, startQuizButton, quizPrompt, quizAnswerInput, submitAnswer,
-    correctAnswerButton, incorrectAnswerButton, quizTitle, quizReviewHeader,
-    quizScore, finishQuizButton, quizBoxDropdown, quizBoxDropdownOnePlus,
-    quizBoxDropdownTwoPlus, quizBoxDropdownThree
+    studyNavButton, startQuizButton, quizAnswerInput, submitAnswer,
+    correctAnswerButton, incorrectAnswerButton,
+    finishQuizButton, quizBoxDropdown, quizBoxDropdownOnePlus,
+    quizBoxDropdownTwoPlus, quizBoxDropdownThree,
   } = selectors;
   await t
-    //Box 1+ Initial
+    // Box 1+ Initial
     .click(studyNavButton)
     .expect(quizBoxDropdown.exists)
     .ok()
@@ -103,14 +104,14 @@ test.before(async (t) => {
     .click(submitAnswer)
     .click(incorrectAnswerButton)
     .click(finishQuizButton)
-    //Check Box 3 is empty
+    // Check Box 3 is empty
     .click(quizBoxDropdown)
     .click(quizBoxDropdownThree)
     .click(startQuizButton)
     .expect(Selector('h3').withText('0 correct out of 0 - NaN%').visible)
     .ok()
     .click(finishQuizButton)
-    //Check Box 2 has two cards and progress
+    // Check Box 2 has two cards and progress
     .click(quizBoxDropdown)
     .click(quizBoxDropdownTwoPlus)
     .click(startQuizButton)
@@ -123,7 +124,7 @@ test.before(async (t) => {
     .click(submitAnswer)
     .click(incorrectAnswerButton)
     .click(finishQuizButton)
-    //Check Box 2+ has one card
+    // Check Box 2+ has one card
     .click(quizBoxDropdown)
     .click(quizBoxDropdownTwoPlus)
     .click(startQuizButton)
@@ -133,7 +134,7 @@ test.before(async (t) => {
     .click(submitAnswer)
     .click(correctAnswerButton)
     .click(finishQuizButton)
-    //Check Box 3 has one card
+    // Check Box 3 has one card
     .click(quizBoxDropdown)
     .click(quizBoxDropdownThree)
     .click(startQuizButton)
@@ -142,8 +143,8 @@ test.before(async (t) => {
     .click(submitAnswer)
     .click(correctAnswerButton)
     .click(finishQuizButton)
-    //Check box 1+ has 3 cards
+    // Check box 1+ has 3 cards
     .click(startQuizButton)
     .expect(Selector('h2').withText('Question 1 of 3').visible)
-    .ok()
+    .ok();
 });
