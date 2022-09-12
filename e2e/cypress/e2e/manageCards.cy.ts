6e37ed9a24aea914c8dc2dc8e51c73f0530f7b15
JRIngram <jr-ingram@hotmail.co.uk>
2022-09-12 10:18:38 +0100

    Explicitly check for text

--- e2e/cypress/e2e/manageCards.cy.ts
+++ e2e/cypress/e2e/manageCards.cy.ts
@@ -6,14 +6,16 @@ describe('manage cards', () => {
   })
 
   it('can add and then delete a card', () => {
-    // manage
+    const promptText = 'What is the latin name for the "Eastern Gray Squirrel"?';
+    const answerText = 'Sciurus carolinensis';
+  
     cy.get(selectors.manageNavButton).click();
     cy.get(selectors.addCardsButton).click();
-    cy.get(selectors.promptInput).type('What is the latin name for the "Eastern Gray Squirrel"?')
+    cy.get(selectors.promptInput).type(promptText)
     cy.get(selectors.answerInput).type('Sciurus carolinensis')
     cy.get(selectors.addCardButton).click()
-    cy.get(selectors.savedPrompt).should('be.visible')
-    cy.get(selectors.savedAnswer);
+    cy.get(selectors.savedPrompt).contains(promptText).should('be.visible')
+    cy.get(selectors.savedAnswer).contains(answerText).should('be.visible');
     cy.get(selectors.deleteCardButton).click()
     cy.get(selectors.savedPrompt).should('not.exist');
     cy.get(selectors.savedAnswer).should('not.exist');
@@ -30,6 +32,9 @@ describe('manage cards', () => {
   });
 
   it('can create, update and delete a card', () => {
+    const promptText = 'Prompt: What is the latin name for the "Tawny Owl"?';
+    const answerText = 'Sciurus carolinensis';
+
     cy.get(selectors.manageNavButton).click();
     cy.get(selectors.addCardsButton).click();
     cy.get(selectors.promptInput).type('Prompt: What is the latin name for the "Owl"?');
@@ -37,14 +42,14 @@ describe('manage cards', () => {
     cy.get(selectors.addCardButton).click();
     cy.get(selectors.editCardButton).click();
     cy.get(selectors.promptInput).clear()
-    cy.get(selectors.promptInput).type('Prompt: What is the latin name for the "Tawny Owl"?');
+    cy.get(selectors.promptInput).type(promptText);
     cy.get(selectors.answerInput).clear()
-    cy.get(selectors.answerInput).type('Strix aluco');
+    cy.get(selectors.answerInput).type(answerText);
     cy.get(selectors.greenEditCardButton).click();
-    cy.get(selectors.savedPrompt).should('be.visible');
-    cy.get(selectors.savedAnswer).should('be.visible');
-    cy.get(selectors.deleteCardButton);
-    cy.get(selectors.savedPrompt).should('be.visible');
-    cy.get(selectors.savedAnswer).should('be.visible');
+    cy.get(selectors.savedPrompt).contains(promptText).should('be.visible');
+    cy.get(selectors.savedAnswer).contains(answerText).should('be.visible');
+    cy.get(selectors.deleteCardButton).click();
+    cy.get(selectors.savedPrompt).should('not.exist');
+    cy.get(selectors.savedAnswer).should('not.exist');
   });
 });
